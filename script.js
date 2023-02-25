a1313ccc06b534e4a0810d0d77e4f6d3c0c802e8
angel <108492882+angel-472@users.noreply.github.com>
2023-02-25 13:15:07 -0400

    Add features to copy and change colors

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const txtCss = document.getElementById("txt-css");
 const colorsContainer = document.getElementById("colors");
 
 txtCss.addEventListener('input', () => {
+  updateColors();
+});
+
+function updateColors(){
   let matches = findColors(txtCss.value);
   let colors = [];
   if(matches !== undefined){
@@ -18,7 +22,7 @@ txtCss.addEventListener('input', () => {
     let element = getColorElement(color);
     colorsContainer.appendChild(element);
   });
-});
+}
 
 function findColors(str) {
   const regex = /#([0-9a-fA-F]{3}){1,2}\b|rgb\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*\)|rgba\(\s*\d+\s*,\s*\d+\s*,\s*\d+\s*,\s*[\d\.]+\s*\)/g;
@@ -28,11 +32,40 @@ function findColors(str) {
 
 function getColorElement(color){
   let element = document.createElement("div");
+  let pickerId = crypto.randomUUID();
   element.classList.add("color");
   element.innerHTML =  `
-  <div class="color-display" style="background-color: ${color};"></div>
+  <input data-color="${color}" class="color-display" type="color" value="${color}">
   <p class="color-name">${color}</p>
-  <button class="button color-change">Replace</button>
+  <button class="button" data-color="${color}">Copy</button>
   `;
   return element;
 }
+
+function replaceColor(oldColor, newColor){
+  txtCss.value = txtCss.value.replaceAll(oldColor, newColor);
+  updateColors();
+}
+
+document.addEventListener('click', ({ target }) => {
+  if (target.matches('button') && target.getAttribute("data-color") !== undefined) {
+    let color = target.getAttribute("data-color");
+    navigator.clipboard.writeText(color);
+    let value = target.innerText;
+    let copiedText = "Copied";
+    if(value !== copiedText){
+      target.innerText = copiedText;
+      setTimeout(() => {
+        target.innerText = value;
+      }, 2000);
+    }
+  }
+});
+
+document.addEventListener('change', ({ target }) => {
+  if (target.matches('input') && target.getAttribute("data-color") !== undefined) {
+    let oldColor = target.getAttribute("data-color");
+    let newColor = target.value;
+    replaceColor(oldColor,newColor);
+  }
+});
